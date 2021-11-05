0ab04db2fec054446c9f30cbdfe15a896e486b86
sharada1022 <landasharada@gmail.com>
2021-11-05 21:49:31 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar,Nav,Container,NavDropdown} from 'react-bootstrap';
-import {Form,Row,Col} from 'react-bootstrap'
+import {Form,Row,Col,Stack  } from 'react-bootstrap';
+import intel from './copy.png';
 
 function App() {
   return (
     <div className="App">
       <Navbar className = "mb-4" collapseOnSelect expand="lg" bg="primary" variant="dark">
-      <Container>
-  <Navbar.Brand href="#home">Intel</Navbar.Brand>
+      <Container className="logo">
+ <img src = {intel}/> <Navbar.Brand href="#home"></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
       <Nav.Link href="#features">Rambo</Nav.Link>  
     </Nav>
-    <Nav>
+    <Nav> 
     <NavDropdown title="Ops Tools" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -49,16 +50,16 @@ function App() {
   </Navbar.Collapse>  
   </Container>
 </Navbar>
-<div className = "" >
+<div className = "mb-2" >
 <Container>  
 <Row >
- <h1 className = "mb-5 test-header">Row/Rack Management</h1>
- <hr />
+ <h1 className = "mb-3 test-header">Row/Rack Management</h1>
+ <hr/>
  </Row>
  </Container>
  </div>
  <Container>
-<Nav className = "mb-5" variant="tabs" defaultActiveKey="/home">
+<Nav className = "mb-3" variant="tabs" defaultActiveKey="/home">
   <Nav.Item>
     <Nav.Link href="/home">View Data</Nav.Link>
   </Nav.Item>
@@ -66,17 +67,13 @@ function App() {
     <Nav.Link eventKey="link-1">Edit Data</Nav.Link>
   </Nav.Item> 
 </Nav>
-</Container>
-<div className = "mb-0.2" >
- <p>All fields optional</p>
- </div>
- <Container>
 <Form>
-<div > 
+<div className = "mb-1"> 
+<p className ="mb-1 test"><b>All fields optional</b></p>
   <Container>
-  <Row className = "mb-3"> 
-    <Col xs={7}>
-      <Form.Group  as={Row} className="mb-3">
+   <Row className = "mb-1"> 
+    <Col xs={6}> 
+      <Form.Group  as={Row} className="mb-1">
       <Form.Label column sm="4">Building</Form.Label>
       <Col sm="8">
       <Form.Control  className = "mb-3" placeholder="fm1" />
@@ -89,27 +86,33 @@ function App() {
       </Col>
       </Form.Group>
     </Col>
-    
-    
     <Col xs={4}>
-      <Form.Label>Row</Form.Label>
+      <Form.Group  as={Row} className="mb-1">
+      <Form.Label column sm="4">Row</Form.Label>
+      <Col sm="">
       <Form.Control className = "mb-3" placeholder="CP" />
-      <Form.Label>Rack</Form.Label>
+      </Col>
+      </Form.Group>
+      <Form.Group  as={Row} className="mb-3">
+      <Form.Label column sm="4">Rack</Form.Label>
+      <Col sm="8">
       <Form.Control placeholder="12" />
+      </Col>
+      </Form.Group>
     </Col>
   </Row>
+   {/* <div  className ="w-responsive text-center mx-auto p-3 mt-2">  */}
+   <Stack gap={2} className="col-md-8 mx-auto">
+      <Button  variant="primary"  size="md">
+      Search
+     </Button>  
+    </Stack>
+  {/* </div>  */}
   </Container>
-  <div  className ="d-grid gap-2"> 
-  
-  <Button  variant="primary" size="md">
-    Search
-  </Button>
-</div>
   </div>  
 </Form>
 </Container>
-    </div>
-    
+    </div> 
   );      
 }
 
